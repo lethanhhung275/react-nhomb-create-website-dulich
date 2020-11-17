57c5227f206d9ede06b1ba953673a3e8b59bf81b
tranconghoaxx <tranhoa300696@gmail.com>
2020-11-17 21:58:39 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-nhomb-create-website-dulich'
+import 'react-nhomb-create-website-dulich/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
